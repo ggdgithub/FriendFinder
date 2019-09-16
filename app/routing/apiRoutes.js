f7de979f875141b2fb25424834fb86120eb3150d
Gabriel de Leon <gabriel.deleon0126@gmail.com>
2019-09-16 10:07:58 -0700

    commit before submission

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -12,29 +12,26 @@ module.exports = function(app) {
     for(var i = 0; i < user.scores.length; i++) {
       user.scores[i] = parseInt(user.scores[i]);
     }
-    
+
     var friendScore = 0;
-    var minimumDifference = 40;
+    var baseScore = 40;
 
 
     for(var i = 0; i < friends.length; i++) {
-      var totalDifference = 0;
+      var totalScore = 0;
       for(var j = 0; j < friends[i].scores.length; j++) {
         var difference = Math.abs(user.scores[j] - friends[i].scores[j]);
-        totalDifference += difference;
+        totalScore += difference;
       }
 
-      // if there is a new minimum, change the best friend index and set the new minimum for next iteration comparisons
       if(totalDifference < minimumDifference) {
         friendScore = i;
-        minimumDifference = totalDifference;
+        baseScore = totalScore;
       }
     }
 
-    // after finding match, add user to friend array
     friends.push(user);
 
-    // send back to browser the best friend match
     res.json(friends[bestFriendIndex]);
   });
 };
